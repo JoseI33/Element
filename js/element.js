a6e4d5d92b0467c60b1587d8c78653e9ceba5c44
USUARIO <jose.imhoff.20@gmail.com>
2024-03-03 00:36:50 -0300

    Creando el juego de los elementos

---
+++ js/element.js
@@ -0,0 +1,6 @@
+function seleccionarMascota() {
+    alert("Seleccionaste a tu mascota!")
+}
+
+let botonMascota =  document.getElementById("boton-mascota");
+botonMascota.addEventListener('click', seleccionarMascota);
